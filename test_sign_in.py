3fd4f16b900a624f37cbbbf3c999f22a9155583a
AhmedCompo <ahmedcompo2010@gmail.com>
2023-07-28 01:12:41 +0300

    Update test_sign_in.py

--- test_sign_in.py
+++ test_sign_in.py
@@ -1,10 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
-options = Options()
-options.add_argument("--headless")
+
 driver = webdriver.Chrome()
-
 
 driver.get("http://selenium.dev")
 
