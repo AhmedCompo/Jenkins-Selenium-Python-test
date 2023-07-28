7985a513e3f21adb9e883ed38d8b022714f32000
AhmedCompo <ahmedcompo2010@gmail.com>
2023-07-28 05:23:34 +0300

    Update test_sign_in.py

--- test_sign_in.py
+++ test_sign_in.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 
 # Use ChromeOptions to specify the Chrome binary location
 chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = "/"  # Replace this with the actual path
+chrome_options.binary_location = "/var/lib/jenkins/.local/bin"  # Replace this with the actual path
 
 # Initialize the WebDriver with Chrome driver and options
 driver = webdriver.Chrome(options=chrome_options)
