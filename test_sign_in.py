34320fcc0628cc1c9e8e3a6044352f64cbb9ec1d
AhmedCompo <ahmedcompo2010@gmail.com>
2023-07-28 22:48:31 +0300

    Update test_sign_in.py

--- test_sign_in.py
+++ test_sign_in.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome()
 
 driver.get("http://www.python.org")
 assert "Python" in driver.title
