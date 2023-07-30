6c54e6884674f7c07e370c7793f921489f00c648
AhmedCompo <ahmedcompo2010@gmail.com>
2023-07-30 23:02:58 +0300

    Update test_sign_in.py

--- test_sign_in.py
+++ test_sign_in.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 options = webdriver.ChromeOptions()
-options.add_arguments("--headless")
+options.add_argument("--headless")
 options.add_argument("--window-size=1920x1080")
 driver = webdriver.Chrome(options=options)
 
