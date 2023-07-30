57b998892c5ca1c70ef4c766a3cceab89bcff6a7
AhmedCompo <ahmedcompo2010@gmail.com>
2023-07-30 15:52:36 +0300

    Update test_sign_in.py

--- test_sign_in.py
+++ test_sign_in.py
@@ -1,6 +1,21 @@
 from selenium import webdriver
+import chromedriver_autoinstaller
 
-driver = webdriver.Chrome()
+def test_google():
+    # Automatically install and use the appropriate ChromeDriver version
+    chromedriver_autoinstaller.install()
 
-driver.get("http://www.python.org")
-assert "Python" in driver.title
+    # Create a Chrome WebDriver instance
+    driver = webdriver.Chrome()
+
+    # Navigate to Google
+    driver.get('https://www.google.com')
+
+    # Do some tests (e.g., check the title)
+    assert "Google" in driver.title
+
+    # Clean up
+    driver.quit()
+
+if __name__ == "__main__":
+    test_google()
