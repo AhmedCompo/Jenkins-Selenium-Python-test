91edbba44c5d09da153ab763d71bdfe184d46c82
AhmedCompo <ahmedcompo2010@gmail.com>
2023-07-30 22:23:57 +0300

    Update test_sign_in.py

--- test_sign_in.py
+++ test_sign_in.py
@@ -1,11 +1,6 @@
 from selenium import webdriver
 options = webdriver.ChromeOptions()
-options.setExperimentalOption("prefs", chromePrefs)
-options.addArguments("--no-sandbox")
-options.addArguments("--headless")
-options.addArguments("--disable-dev-shm-usage")
-options.addArguments("--window-size=1920x1080")
-driver = new ChromeDriver(options);
+driver = webdriver.Chrome(options=options)
 
 #driver = webdriver.Chrome()
 
