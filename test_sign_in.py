c45a7a7976ca99479f86a3b74718f4a223acde19
AhmedCompo <ahmedcompo2010@gmail.com>
2023-07-30 22:17:06 +0300

    Update test_sign_in.py

--- test_sign_in.py
+++ test_sign_in.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
-options = new ChromeOptions();
-options.setExperimentalOption("prefs", chromePrefs);
-options.addArguments("--no-sandbox");
-options.addArguments("--headless"); //!!!should be enabled for Jenkins
-options.addArguments("--disable-dev-shm-usage"); //!!!should be enabled for Jenkins
-options.addArguments("--window-size=1920x1080"); //!!!should be enabled for Jenkins
+options = webdriver.ChromeOptions()
+options.setExperimentalOption("prefs", chromePrefs)
+options.addArguments("--no-sandbox")
+options.addArguments("--headless")
+options.addArguments("--disable-dev-shm-usage")
+options.addArguments("--window-size=1920x1080")
 driver = new ChromeDriver(options);
 
 #driver = webdriver.Chrome()
